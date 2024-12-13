7d3d2dc86c5ebc5a02c99b1bff43150c7cd8b6f3
trophate <627706327@qq.com>
2024-12-13 17:31:40 +0800

    init

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<script setup>
+let username;
+let password;
+</script>
+
+<template>
+  <div>
+    <input :value="username" />
+  </div>
+  <div>
+    <input type="password" :value="password"  />
+  </div>
+  <button>登录</button>
+</template>
+
+<style scoped>
+
+</style>
