48a3ab6d422703d846c857c4c95a03b254b8f530
trophate <627706327@qq.com>
2024-12-20 16:30:49 +0800

    feat:

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import Login from "./components/Login.vue";
+import Login from "./components/Login.vue"
 
 createApp(Login).mount('#login')
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://43.136.216.84:41730',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    }
+  }
 })
