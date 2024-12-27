6b2cc46b6ed67e633ab344d1a66448e24d114e09
trophate <627706327@qq.com>
2024-12-27 17:31:02 +0800

    feat: 新增头组件。

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import AccountCircle from "../icon/AccountCircle.vue";
+</script>
+
+<template>
+  <v-row id="headerBack">
+    <v-col id="headerLeft">
+
+    </v-col>
+    <v-col id="headerRight" class="offset-11">
+      <account-circle />
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+  #headerBack {
+    height: 130px;
+  }
+  #headerRight {
+    margin-top: 70px;
+  }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Header from "../components/Header.vue";
+</script>
+
+<template>
+  <v-container>
+    <Header />
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
