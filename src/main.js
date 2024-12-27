6693720cd74c4aac79bc3702dab4f917c1b5e174
trophate <627706327@qq.com>
2024-12-27 11:36:46 +0800

    build: 引入vuetify。

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import "./plugins/route.js"
 import { createApp } from 'vue'
 import { router } from "./plugins/route.js"
+import { vuetify } from "./plugins/vuetify.js"
 
 import App from "./App.vue"
 
 createApp(App)
+  .use(vuetify)
   .use(router)
   .mount('#app')
